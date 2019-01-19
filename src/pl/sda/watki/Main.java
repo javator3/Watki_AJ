bdc7d958c3565e8b6bedf125b381e1b017dd6b82
Adam Jagła <jagla1990@gmail.com>
2019-01-19 12:03:05 +0100

    Runner Example

--- src/pl/sda/watki/Main.java
+++ src/pl/sda/watki/Main.java
@@ -1,7 +1,34 @@
 package pl.sda.watki;
 
+class Runner implements  Runnable{
+    long silnia(long n){
+        if(n<2){
+            return 1;
+        }
+        return silnia(n-1) * 1;
+    }
+
+    @Override
+    public void run() {
+
+        for(int i=0; i<500;i++){
+            System.out.println("Hello: " + " thread: " + Thread.currentThread().getName());
+
+        silnia(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
+        Thread thread = new Thread(new Runner());
+        Thread thread1 = new Thread(new Runner());
+        thread.start();
+        thread1.start();
     }
 }
---
+++ src/pl/sda/watki/RunnerExample.java
@@ -0,0 +1,17 @@
+package pl.sda.watki;
+
+public class RunnerExample extends  Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello: " + " thread: " + Thread.currentThread().getName());
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
